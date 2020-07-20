0da8e41ca0ddd9c0faa22cd6fcad7a77eb221cd5
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2020-07-19 21:57:25 -0300

    Create while_vs_for.c

---
+++ while_vs_for.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Using for */
+int main()
+{
+int i;
+for (i=1; i<=10 ; i = i + 1)
+    printf("%d\n",i);
+    return 0;
+}
+
+/* Using while
+
+int i;
+i=1;
+while(i<=10)
+{
+printf("%d\n",i);
+i = i + 1;
+}
+}
+*/
