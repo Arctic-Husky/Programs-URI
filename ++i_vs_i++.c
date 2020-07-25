ee42fb485cfc4c9ce609fa46f2c3512ddba884c5
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2020-07-25 10:31:57 -0300

    Create ++i_vs_i++.c

---
+++ ++i_vs_i++.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i; /*This is just to show the difference between ++ or -- before or after the variable*/
+    i=2;
+    printf("%d\n",i--);
+    printf("%d\n",i);
+    printf("%d\n",--i);
+return 0;
+}
