cbf162076d7e3c967f3c957927f7e670b9dec8dc
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2020-09-08 07:52:42 -0300

    Create gets_vs_scanf.c

---
+++ gets_vs_scanf.c
@@ -0,0 +1,23 @@
+int main()
+{
+    char nome[50];
+    gets(nome);                 <- gets everything I put
+    printf("%s\n",nome);
+
+}
+INPUT: ZABUZA MOMOCHI
+OUTPUT: ZABUZA MOMOCHI
+
+
+
+
+
+int main()
+{
+    char nome[50];
+    scanf("%s",nome);           <- gets only one word
+    printf("%s\n",nome);
+
+}
+INPUT: ZABUZA MOMOCHI
+OUTPUT: ZABUZA
