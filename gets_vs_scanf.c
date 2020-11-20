b78d9ed5a7ac415b6c60e449d20ef8471992dda0
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2020-11-20 08:49:43 -0300

    Update gets_vs_scanf.c

--- gets_vs_scanf.c
+++ gets_vs_scanf.c
@@ -1,12 +1,12 @@
 int main()
 {
     char nome[50];
-    gets(nome);                 <- gets everything I put
+    gets(nome);              //   <- gets everything I put
     printf("%s\n",nome);
 
 }
-INPUT: ZABUZA MOMOCHI
-OUTPUT: ZABUZA MOMOCHI
+//INPUT: ZABUZA MOMOCHI
+//OUTPUT: ZABUZA MOMOCHI
 
 
 
@@ -15,20 +15,21 @@ OUTPUT: ZABUZA MOMOCHI
 int main()
 {
     char nome[50];
-    scanf("%s",nome);           <- gets only one word
+    scanf("%s",nome); //          <- gets only one word
     printf("%s\n",nome);
 
 }
-INPUT: ZABUZA MOMOCHI
-OUTPUT: ZABUZA
+//INPUT: ZABUZA MOMOCHI
+//OUTPUT: ZABUZA
 
-HOW TO FIX THIS :
+//HOW TO FIX THIS :
 
 int main()
 {
     char str[11];
 
-    scanf(" %10[^\n]", str);          <- gets everything up to 10 characters, and if it finds a \n in the string (when you press enter), it stops printing
+    scanf(" %10[^\n]", str); //         <- gets everything up to 10 characters, and if it finds a \n in the string (when you press enter), it stops printing
+                             //         ^ means negation
 
     printf("%s",str);
 }
