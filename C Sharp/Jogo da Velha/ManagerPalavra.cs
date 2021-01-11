d540018e015381ce4c648ccc878454cb864b62fc
Caio Schmidt França Fonseca <caiofonseca6@gmail.com>
2021-01-11 17:55:03 -0300

    Add files via upload

---
+++ C Sharp/Jogo da Velha/ManagerPalavra.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_Forca_2
+{
+    class ManagerPalavra
+    {
+        private int qtdChars;
+        private int qtdLetras = 0;
+        private int vidas = 6;
+        private String morreu;
+        public String Palavra { get; private set; }
+        public ManagerPalavra(String palavra)
+        {
+            this.Palavra = palavra;
+            qtdChars = Palavra.Length;
+        }
+
+        private Char[] palavraChars = new char[100];
+
+        public Char[] getPalavraChars()
+        {
+            for(int i = 0; i < Palavra.Length; i++)
+            {
+                palavraChars[i] = Palavra[i];
+            }
+
+            return palavraChars;
+        }
+
+        public int getQtdChars()
+        {
+            qtdChars = Palavra.Length;
+            return qtdChars;
+        }
+
+        public int getQtdLetras()
+        {
+            for(int i = 0; i < Palavra.Length; i++)
+            {
+                if((Palavra[i] >= 65 && Palavra[i] <= 90) || (Palavra[i] >= 97 && Palavra[i] <= 122))
+                        qtdLetras++;
+            }
+
+            return qtdLetras;
+        }
+
+        public String getMorreu()
+        {
+            if (vidas == 6)
+                morreu = "      ";
+            else if (vidas >= 5)
+                morreu = "P     ";
+            else if (vidas >= 4)
+                morreu = "PE    ";
+            else if (vidas >= 3)
+                morreu = "PER   ";
+            else if (vidas >= 2)
+                morreu = "PERD  ";
+            else if (vidas >= 1)
+                morreu = "PERDE ";
+            else if (vidas >= 0)
+                morreu = "PERDEU";
+
+            return morreu;
+        }
+
+        public int getVidas()
+        {
+            return vidas;
+        }
+
+        public void perdeVida()
+        {
+            vidas--;
+        }
+    }
+}
---
+++ C Sharp/Jogo da Velha/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jogo_da_Forca_2
+{
+    /* 
+     A fazer:
+
+            1- Mestre escreve uma palavra
+            2- Ela aparece na tela porem na forma de _ _ _ _ _
+            3- O jogador digita uma letra
+            4- Letra aparece no lugar do _ correspondente
+            5- Caso o numero de tentativas acabe o jogo termina
+            6- Caso a palavra seja formada por inteiro o jogo termina
+
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("\n\n\tMestre, escreva a palavra sem acentos: ");
+            String palavra = Console.ReadLine();
+            Console.Clear();
+            int encontrouTotal = 0;
+            bool venceu = false;
+            char letra;
+
+            ManagerPalavra manager = new ManagerPalavra(palavra);
+
+            Char[] encriptada = new char[(manager.getQtdChars())*2];
+
+            for(int i = 0; i < (manager.getQtdChars())*2; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    encriptada[i] = ' ';
+                }
+                else
+                    encriptada[i] = '_';
+            }
+
+            while(manager.getVidas() > 0)
+            {
+                String morreu = manager.getMorreu();
+                Char[] palavraChars = manager.getPalavraChars();
+                bool encontrou = false;
+                //String palavraInteira;
+
+                Console.Write("\n\n\t");
+                Console.Write(encriptada);
+                Console.Write("     ");
+                Console.Write(morreu);
+                Console.Write("\n\n");
+                Console.Write("\n\tInsira uma letra: ");
+                letra = Console.ReadKey().KeyChar;
+                /*if (letra == '0')
+                {
+                    Console.Write("\n\n\tInsira a palavra: ");                  <--- Minha tentativa de fazer com que o jogador possa adivinhar a palavra inteira
+                    palavraInteira = Console.ReadLine();
+                    if (palavraInteira == palavra)
+                    {
+                        encontrou = true;
+                        break;
+                    }
+                    else
+                        break;
+                }
+                else
+                    continue;*/
+                Console.Write("\n\n");
+
+                for (int i = 0; i < manager.getQtdChars(); i++)
+                {
+                    if (palavraChars[i] == letra)
+                    {
+                        if (encriptada[(i * 2) + 1] == letra) // Caso a letra ja tenha sido digitada, break, o jogador perde uma "vida"
+                            break;
+                        encriptada[(i * 2) + 1] = letra;
+                        encontrou = true;
+                        encontrouTotal++;
+                    }
+                }
+
+                if (!encontrou)
+                    manager.perdeVida();
+                if(encontrouTotal == manager.getQtdChars())
+                {
+                    venceu = true;
+                    break;
+                }
+
+                Console.Clear();
+
+            }
+
+            Console.Write("\n\n\t");
+            Console.Write(encriptada);
+            Console.Write("     ");
+            Console.Write(manager.getMorreu());
+            Console.Write("\n\n");
+
+            if (venceu)
+            {
+                Console.Write("\n\n\tParabéns, você adivinhou a palavra!\n\n");
+            }
+            else if(!venceu)
+            {
+                Console.Write("\n\n\tVocê perdeu... mais sorte na proxima!\n\n");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
